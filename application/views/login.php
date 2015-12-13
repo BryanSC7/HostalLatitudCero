71f4c4ae4f8971973fa1a514c27941c7365f4e79
Bryan Suarez <bryan@Personal>
2015-12-13 00:29:47 -0500

    Avances en cambio de imagen de fondo en backdound , aun queda ajustar imagen Implementación del boton cancelar login

--- application/views/login.php
+++ application/views/login.php
@@ -4,6 +4,19 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSS
+        ================================================== -->
+        <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic%7CMontserrat:400,700" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/flexslider.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/font-awesome.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/weather-icons.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/prettyPhoto.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/mmenu.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/jquery-ui-1.10.4.datepicker.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/style.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/css/mediaqueries.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plantilla/colors/default.css">
 
         <!-- Favicons
             ================================================== -->
@@ -13,7 +26,7 @@
         <link rel="apple-touch-icon" sizes="114x114" href="#">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Login</title>
-    <center><h1>Bienvenido al Sistema</h1></center>
+    <center><h1 id="titulos">Bienvenido al Sistema</h1></center>
 
     <!-- Bootstrap -->
     <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
@@ -25,28 +38,29 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body>
+<body background="<?php echo base_url(); ?>assets/images/hotel2.jpg" >
     <div class="container">
         <div class="row">
-            <div class="panel panel-primary" >
-                <span class="glyphicon glyphicon-user"></span>
-                <div class="panel-body">
-                    <div class="col-md-4 col-md-offset-4">
-                        <h3>Ingresar Credenciales</h3>
-                        <form action=" " method="POST">
-                            <div class="form-group">
-                                <label for="username"></label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="Nombre de Usuario">
-                            </div>
-                            <div class="form-group">
-                                <label for="password"></label>
-                                <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña">
-                            </div>
-                            <input  type="submit" value="Ingresar" class="btn btn-success">
-                        </form>
-                    </div>
+            <!-- <div class="panel panel-primary" >-->
+            <span class="glyphicon glyphicon-user"></span>
+            <div class="panel-body">
+                <div class="col-md-4 col-md-offset-4">
+                    <h3 id="credenciales">Ingresar Credenciales</h3>
+                    <form action=" " method="POST">
+                        <div class="form-group">
+                            <label for="username"></label>
+                            <input type="text" name="username" id="username" class="form-control" placeholder="Nombre de Usuario">
+                        </div>
+                        <div class="form-group">
+                            <label for="password"></label>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña">
+                        </div>
+                        <input  type="submit" value="Ingresar" class="btn btn-success">
+                        <a href="<?php echo base_url('login/logout'); ?>"type="submit" class="btn btn-danger"> Cancelar </a>
+                    </form>
                 </div>
             </div>
+            <!--</div>-->
         </div>
     </div>
 </body>
