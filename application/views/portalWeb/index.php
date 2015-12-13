86b0a24461b7bbbc71083514d38c4ca09c194b7c
user <user@user-PC>
2015-12-13 09:40:31 -0500

    Hice un Hola para probar el versionado de edison

--- application/views/portalWeb/index.php
+++ application/views/portalWeb/index.php
@@ -2,7 +2,7 @@
 <!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
 <head>
-
+        <!--Prueba de Commit-->
 	<!-- Basic Page Needs
 	================================================== -->
 	<meta charset="utf-8">
